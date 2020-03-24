94f502dcabc06c2a88799ab685ffd491fa590780
Chris Herbert <herbert@bermanco.com>
2020-03-24 19:10:55 -0400

    Email address no longer required

--- setup/Theme/CustomFieldsACF.php
+++ setup/Theme/CustomFieldsACF.php
@@ -25,7 +25,7 @@ class CustomFieldsACF {
 					'name' => 'email',
 					'type' => 'email',
 					'instructions' => '',
-					'required' => 1,
+					'required' => 0,
 					'conditional_logic' => 0,
 					'wrapper' => array(
 						'width' => '',
